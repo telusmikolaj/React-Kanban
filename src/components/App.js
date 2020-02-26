d95fff5c2a11334eb05628c2836395b55b15ee8d
telusmikolaj <mikolaj.telus@gmail.com>
2020-02-26 06:49:21 +0100

    removeuseless files

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,8 @@
 // ./src/components/App.js
 import React from 'react';
-import Box from './Box';
 
-class App extends React.Component {
-    state = {
-        text: 'React HelloWorld Modern!',
-    };
-
-    render() {
-        const { text } = this.state;
-
-        return <Box text={text} />;
-    }
-}
+const App = () => {
+    return <h1>Hello World</h1>;
+};
 
 export default App;
